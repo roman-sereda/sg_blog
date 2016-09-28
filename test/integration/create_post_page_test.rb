58719101218ce04889461c540e61141dd2f185a1
roman-sereda <Romansereda98@gmail.com>
2016-09-28 09:50:31 +0300

    CreatePost added

--- test/integration/create_post_page_test.rb
+++ test/integration/create_post_page_test.rb
@@ -18,12 +18,11 @@ class CreatePostPageTest < ActiveSupport::TestCase
   def test_enter_true_data_and_see_created_post
 
     fill_in("title_field", with: "The_unique_title")
-    fill_in("body_field", with: 'Washington, D.C., formally
-    the District of Columbia and commonly referred to as "Washington",
-    "the District", or simply "D.C.", is the capital of the United States.
-    The signing of the Residence Act on July 16, 1790, approved the creation
-    of a capital district located along the Potomac River on the countrys
-    East Coast.')
+    fill_in("body_field", with: 'Washington, D.C., formally the District of
+  Columbia and commonly referred to as "Washington", "the District", or
+   simply "D.C.", is the capital of the United States. The signing of the
+   Residence Act on July 16, 1790, approved the creation of a capital
+   district located along the Potomac River on the countrys East Coast.')
     click_button "Create new post"
     assert page.has_content? "The_unique_title"
 
