f6a957a262e321a70d4efa1a7bbe7926e8d7952b
roman-sereda <Romansereda98@gmail.com>
2016-10-07 21:18:32 +0300

    Added post.scss and style to all views

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
 
   validates_presence_of :title, :body
-  validates :title, length: {minimum: 5, maximum: 20}
+  validates :title, length: {minimum: 5, maximum: 100}
   validates :body, length: {minimum: 200}
 
 end
