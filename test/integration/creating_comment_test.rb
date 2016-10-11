f5ed0bd0385604283e5fc6a4261395bed122de92
roman-sereda <Romansereda98@gmail.com>
2016-10-11 19:07:31 +0300

    Added tests for comments

---
+++ test/integration/creating_comment_test.rb
@@ -0,0 +1,29 @@
+require "test_helper"
+
+class CreatingCommentTest < ActiveSupport::TestCase
+  def setup
+    Post.create title: "AAAAAAAAAA", body: "A"*200, image: File.open("public/uploads/post/test_images/1.jpg")
+    visit "/posts/1"
+  end
+
+  def test_see_create_new_post_button
+    assert page.has_button?"Create new comment"
+  end
+
+  def test_create_post_and_watch_body
+    fill_in 'comment-area', with: "test-comment"
+    click_button "Create new comment"
+
+    assert page.has_selector?"#comment-area"
+    assert page.has_content?"test-comment"
+  end
+
+  def test_comment_counter
+    assert page.has_content?"Comments: 0"
+
+    fill_in 'comment-area', with: "test-comment"
+    click_button "Create new comment"
+
+    assert page.has_content?"Comments: 1"
+  end
+end
