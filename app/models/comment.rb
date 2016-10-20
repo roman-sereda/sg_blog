76d08b58320ce61c831e490c0068af375b66c207
roman-sereda <Romansereda98@gmail.com>
2016-10-20 18:36:03 +0300

    Validatin comment in model only

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :posts
 
-  validates :body, length: {minimum: 5, maximum: 300}
+  VALIDATION_REGEXP =  /A((http | https | ftp)?:\/\/)?/
+  VALIDATION_FIST_POST = /A(First Post | first post)/
+
+  validates :body, length: {minimum: 2, maximum: 300}
+  validates :body, format: { with: VALIDATION_REGEXP}
+  validates :body, format: { with: VALIDATION_FIST_POST}
 end
