8a3ad642086f1509fd84213663d54eb30aae3478
roman-sereda <Romansereda98@gmail.com>
2016-10-20 21:15:27 +0300

    Fixe regex

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :posts
 
-  VALIDATION_FIST_POST = /\A(Post first | post first)/
-  VALIDATION_REGEXP = /\A(http | https)/
+  VALIDATION_FIST_POST = /First post/
+  VALIDATION_LINK = /(([a-z0-9\-\.]+)?[a-z0-9\-]+(!?\.[a-z]{2,4}))/
 
-  validates :body, length: {minimum: 2, maximum: 300}, format: { with: VALIDATION_FIST_POST}
-  validates :body, format: { with: VALIDATION_REGEXP}
+  validates :body, length: {minimum: 2, maximum: 300}
+  validates :body, format: { without: VALIDATION_FIST_POST}
+  validates :body, format: { without: VALIDATION_LINK}
 end
