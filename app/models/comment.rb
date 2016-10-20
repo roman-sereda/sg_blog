1453f718c17e5a7f7b4ffdbeafb18438b815f6f5
roman-sereda <Romansereda98@gmail.com>
2016-10-20 20:37:25 +0300

    Rewrite validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :posts
 
-  VALIDATION_REGEXP =  /A((http | https | ftp)?:\/\/)?/
-  VALIDATION_FIST_POST = /A(First Post | first post)/
+  VALIDATION_FIST_POST = /\A(Post first | post first)/
+  VALIDATION_REGEXP = /\A(http | https)/
 
-  validates :body, length: {minimum: 2, maximum: 300}
+  validates :body, length: {minimum: 2, maximum: 300}, format: { with: VALIDATION_FIST_POST}
   validates :body, format: { with: VALIDATION_REGEXP}
-  validates :body, format: { with: VALIDATION_FIST_POST}
 end
