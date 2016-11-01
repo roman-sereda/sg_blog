b7624b9b46c90750c245cfa131b78c71805f818c
roman-sereda <Romansereda98@gmail.com>
2016-11-01 23:01:12 +0200

    Add tokens migradition, migrate db to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161101153117) do
+ActiveRecord::Schema.define(version: 20161101202007) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "body"
@@ -35,8 +35,10 @@ ActiveRecord::Schema.define(version: 20161101153117) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
