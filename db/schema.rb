c78db43665a209e49b5feb082cb0956074956769
roman-sereda <Romansereda98@gmail.com>
2016-11-02 23:29:55 +0200

    Create migration for admin to users and update scheme

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161101202007) do
+ActiveRecord::Schema.define(version: 20161102211559) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "body"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20161101202007) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "remember_token"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
