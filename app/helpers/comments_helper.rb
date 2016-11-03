26c7cae32c046477612f4beee11f91e8d881d14f
roman-sereda <Romansereda98@gmail.com>
2016-11-03 19:03:39 +0200

    Edit session and user helpers

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,5 +1,5 @@
 module CommentsHelper
   def comment_body comment
-    "<div class='comment panel panel-default'><p class='comment-created-at'>#{comment.created_at.strftime("%H:%M %Y-%m-%d")}</p><p class='comment-body'>#{comment.body}</p></div>".html_safe
+    "<div class='comment panel panel-default'><p class='comment-created-at'>#{}#{comment.created_at.strftime("%H:%M %Y-%m-%d")}</p><p class='comment-body'>#{comment.body}</p></div>".html_safe
   end
 end
